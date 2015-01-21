d49cf4364fbdb9e045db1b0b2e3272bb7fae8c33
Leonardo Comelli <leonardo.comelli@gmail.com>
2015-01-21 12:05:33 -0200

    changes based on the comments @sivel

--- source_control/gh_keys.py
+++ source_control/gh_keys.py
@@ -20,13 +20,12 @@
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 
 import json
-import requests
-from ansible.module_utils.basic import *
+import base64
 
 DOCUMENTATION = '''
 ---
 module: gh_keys
-version_added: n/a
+version_added: 1.9 
 short_description: Manages github ssh keys.
 description:
     - The module manages the ssh key for a specific user through Github API v3.
@@ -34,54 +33,37 @@ options:
   action:
     description: This tells the gh_keys module what you want it to do.
     required: true
-    choices: 
-      list_keys:
-        description: lists keys for authenticated or non-authenticated user. If the passwd field is informed, the module returns all keys and all data about its (In this case, the module will send the authentication data in request). Otherwise, the module returns only some data about the ssh keys of non-authenticated user.
-        required_fields: [user | user & passwd]
-      get_key:
-        description: gets a specific key by github ssh-key-id
-        required_fields: [user & passwd & key_id]
-      add_key:
-        description: adds a new public ssh key
-        required_fields: [user & passwd & title & key]
-      remove_key:
-        description: removes a specific key by github ssh-key-id
-        required_fields: [user & passwd & key_id]
+    choices: [ list_keys, get_key, add_key, remove_key ]
   user:
     description: Github username.
     required: true
-  passwd:
-    description: Github password. If 2FA is enabled for your account, you should generate a new personal access token. 
-    required: for actions: list_keys (your), get_key, add_key, remove_key
+  password:
+    description: Github password. If 2FA is enabled for your account, you should generate a new personal access token. Required for get_key, add_key and remove_key 
+    required: false 
   title:
-    description: Title of the new ssh key
-    required: for add_key action
+    description: Title of the new ssh key. Required for add_key
+    required: false
   key:
-    description: The path of file that contains the public key
-    required: for add_key action
+    description: The path of file that contains the public key. Required for add_key
+    required: false
   key_id:
-    description: The key id provided by Github
-    required: for get_key and remove_key actions
+    description: The key id provided by Github. Required for get_key and remove_key
+    required: false
 author: Leonardo Comelli
 '''
 
 EXAMPLES = '''
 # Example from Ansible Playbooks
-
 # Lists limit informations about all keys of non-authenticated user
 - gh_keys: action=list_keys user=leocomelli
-
 # Lists all informations about all keys of authenticated user
-- gh_keys: action=list_keys user=leocomelli passwd=secret
-
+- gh_keys: action=list_keys user=leocomelli password=secret
 # Gets a single public key (authetication required)
-- gh_keys: action=get_key user=leocomelli passwd=secret key_id=8767854
-
+- gh_keys: action=get_key user=leocomelli password=secret key_id=8767854
 # Adds a new public key (authentication required)
-- gh_keys: action=add_key user=leocomelli passwd=secret title=my_new_key key=/home/leocomelli/.ssh/id_rsa.pub
-
+- gh_keys: action=add_key user=leocomelli password=secret title=my_new_key key=/home/leocomelli/.ssh/id_rsa.pub
 # Removes an existing ssh key
-- gh_keys: action=remove_key user=leocomelli passwd=secret key_id=8767854
+- gh_keys: action=remove_key user=leocomelli password=secret key_id=8767854
 '''
 
 GH_API_URL = "https://api.github.com/%s"
@@ -89,14 +71,15 @@ GH_API_URL = "https://api.github.com/%s"
 class GHKeys(object):
 
   def __init__(self, module):
+    self.module = module
     self.action = module.params['action']
     self.user   = module.params['user']
-    self.passwd = module.params['passwd']
+    self.password = module.params['password']
     self.title  = module.params['title']
     self.key    = module.params['key']
     self.key_id = module.params['key_id']
   
-  def work(self):
+  def perform_by_action(self):
     return {
       'list_keys'  : self.list_keys,
       'get_key'    : self.get_key,
@@ -105,73 +88,89 @@ class GHKeys(object):
     }[self.action]()
 
   def list_keys(self):
-    if self.passwd is None:
+    if self.password is None:
       url = GH_API_URL % "users/%s/keys" % self.user
-      response = requests.get(url)  
+      response, info = fetch_url(self.module, url)
     else:
       url = GH_API_URL % "user/keys"
-      response = requests.get(url, auth=(self.user, self.passwd))  
-
-    return response.text
-
+      headers = self.get_auth_header(self.user, self.password)
+      response, info = fetch_url(self.module, url, headers=headers)
+    
+    return self.handle_response(response, info)
+ 
   def get_key(self):
-    self.validate_fileds('get_key', ['key_id', 'passwd'])    
+    self.validate_fields('get_key', ['key_id', 'password'])    
 
     url = GH_API_URL %  "user/keys/%s" % self.key_id
-    response = requests.get(url, auth=(self.user, self.passwd)) 
+    headers = self.get_auth_header(self.user, self.password)
+    response, info = fetch_url(self.module, url, headers=headers)
 
-    return response.text
+    return self.handle_response(response, info)
 
   def add_key(self):
-    self.validate_fileds('add_key', ['title', 'key', 'passwd'])
+    self.validate_fields('add_key', ['title', 'key', 'password'])
 
-    with open(self.key, 'r') as content_file:
-      content = content_file.read()
-
+    f = open(self.key, 'r')
+    try:
+      content = f.readline()
+    finally:
+      f.close()
+    
     url = GH_API_URL %  "user/keys"
+    headers = self.get_auth_header(self.user, self.password)
     data = json.dumps({ 'title' : self.title, 'key' : content })
-    response = requests.post(url, auth = (self.user, self.passwd), data = data) 
-
-    return response.text
+    response, info = fetch_url(self.module, url, headers=headers, data=data, method='POST')
+      
+    return self.handle_response(response, info)
 
   def remove_key(self):
-    self.validate_fileds('remove_key', ['key_id', 'passwd'])
+    self.validate_fields('remove_key', ['key_id', 'password'])
 
     url = GH_API_URL %  "user/keys/%s" % self.key_id
-    response = requests.delete(url, auth=(self.user, self.passwd)) 
+    headers = self.get_auth_header(self.user, self.password)
+    response, info = fetch_url(self.module, url, headers=headers, method='DELETE')
 
-    return response.text
+    return self.handle_response(response, info)
 
-  def validate_fileds(self, action, fields):
+  def get_auth_header(self, user, password):
+    auth = base64.encodestring('%s:%s' % (user, password)).replace('\n', '')
+    headers = {
+      'Authorization': 'Basic %s' % auth,
+    }
+    return headers
+
+  def validate_fields(self, action, fields):
     for field in fields:
       if getattr(self, field) is None:
         raise ValueError(field + " cannot be null for action [" + action + "]")
 
-
-def convert_bool_to_str(value):
-  return value.replace('true', '"true"').replace('false', '"false"')
+  def handle_response(self, response, info):
+    if info['status'] == 200:
+      return response.read()
+    else:
+      raise RuntimeError(info['msg']) 
 
 def main():
   module = AnsibleModule(
     argument_spec = dict(
-      passwd = dict(),
-      title  = dict(),
-      key    = dict(),
-      key_id = dict(),
-      user   = dict(required=True),
-      action = dict(required=True, choices=['list_keys', 'get_key', 'add_key', 'remove_key']),
+      password = dict(),
+      title    = dict(),
+      key      = dict(),
+      key_id   = dict(),
+      user     = dict(required=True),
+      action   = dict(required=True, choices=['list_keys', 'get_key', 'add_key', 'remove_key']),
     )
   )  
 
   gh_keys = GHKeys(module)
   try:
-    result = gh_keys.work()
-    if 'message' in result:
-      raise RuntimeError(result)
-
-    module.exit_json(changed=True, stdout=result)
-  except (RuntimeError, ValueError) as err:
+    response = gh_keys.perform_by_action()
+    module.exit_json(changed=True, result=response)
+  except (RuntimeError, ValueError), err:
     module.fail_json(msg=err.args)
+
+from ansible.module_utils.basic import *
+from ansible.module_utils.urls import *
 
 if __name__ == '__main__':
   main()
