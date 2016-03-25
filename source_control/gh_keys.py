bfdf812703434cce627016e5d8eed70572c40f83
Leonardo Comelli <leonardo.comelli@gmail.com>
2016-03-25 16:29:37 -0300

    some adjustments suggested by @resmo

--- source_control/gh_keys.py
+++ source_control/gh_keys.py
@@ -22,37 +22,44 @@
 DOCUMENTATION = '''
 ---
 module: gh_keys
-version_added: 2.1 
-short_description: Manages github ssh keys.
+version_added: "2.1" 
+short_description: Manages GitHub ssh keys.
 description:
-    - The module manages the ssh key for a specific user through Github API v3.
+  - The module manages the ssh key for a specific user through Github API v3.
 
 options:
-  action:
-    description: This tells the gh_keys module what you want it to do.
+  state:
+    description: 
+      - This tells the gh_keys module what you want it to do.
     choices: [ present, absent ]
+    default: present
 
   user:
-    description: Github username.
+    description: 
+      - Github username.
     required: true
 
   password:
-    description: Github password. If 2FA is enabled for your account, you should generate a new personal access token. Required for get_key, add_key and remove_key 
-    required: false
+    description: 
+      - Github password. If 2FA is enabled for your account, you should generate a new personal access token.
+    required: true
     default: none
 
   title:
-    description: Title of the new ssh key. Required for add_key
+    description: 
+      - Title of the new ssh key. Required for the present state
     required: false
     default: none
 
   key:
-    description: The path of file that contains the public key. Required for add_key
+    description: 
+      - The path of file that contains the public key. Required for the present state
     required: false
     default: none
 
   key_id:
-    description: The key id provided by Github. Required for get_key and remove_key
+    description: 
+      - The key id provided by Github. Required for the absent state
     required: false
     default: none
 
@@ -62,34 +69,27 @@ author: Leonardo Comelli (@leocomelli)
 EXAMPLES = '''
 # Example from Ansible Playbooks
 
-# Lists limit informations about all keys of non-authenticated user
-- gh_keys: user=leocomelli
+# Adds a new public key
+- gh_keys: state=present user=leocomelli password=secret title=my_new_key key=/home/leocomelli/.ssh/id_rsa.pub
 
-# Lists all informations about all keys of authenticated user
-- gh_keys: user=leocomelli password=secret
-
-# Gets a single public key (authetication required)
-- gh_keys: user=leocomelli password=secret key_id=8767854
-
-# Adds a new public key (authentication required)
-- gh_keys: action=present user=leocomelli password=secret title=my_new_key key=/home/leocomelli/.ssh/id_rsa.pub
-
-# Removes an existing ssh key (authentication required)
-- gh_keys: action=absent user=leocomelli password=secret key_id=8767854
+# Removes an existing ssh key
+- gh_keys: state=absent user=leocomelli password=secret key_id=8767854
 '''
 
 RETURN = '''
 output:
-  description: the data returned by Github according to the action performed
+  description: the data returned by Github according to the state
   returned: success
   type: dict
   sample: 
     {
-      "id":9999999991,
-      "key":"ssh-rsa AAA2...",
-      "url":"https://api.github.com/user/keys/9999999991",
-      "title":"my-new-pkey",
-      "verified":true
+        "created_at": "2016-03-25T17:08:51Z", 
+        "id": 12345678, 
+        "key": "ssh-rsa AAAA...", 
+        "read_only": false, 
+        "title": "devel", 
+        "url": "https://api.github.com/user/keys/12345678", 
+        "verified": true
     }
 '''
 
@@ -105,45 +105,20 @@ class GHKeys(object):
 
   def __init__(self, module):
     self.module = module
-    self.action = module.params['action']
+    self.state = module.params['state']
     self.user   = module.params['user']
     self.password = module.params['password']
     self.title  = module.params['title']
     self.key    = module.params['key']
     self.key_id = module.params['key_id']
   
-  def perform_by_action(self):
-    print self.action
+  def handle_state(self):
     return {
-      'list_keys'  : self.list_keys,
-      'get_key'    : self.get_key,
-      'present'    : self.add_key,
-      'absent' : self.remove_key
-    }[self.action]()
-
-  def list_keys(self):
-    if self.password is None:
-      url = GH_API_URL % "users/%s/keys" % self.user
-      response, info = fetch_url(self.module, url)
-    else:
-      url = GH_API_URL % "user/keys"
-      headers = self.get_auth_header(self.user, self.password)
-      response, info = fetch_url(self.module, url, headers=headers)
-    
-    return self.handle_response(response, info)
- 
-  def get_key(self):
-    self.validate_fields('get_key', ['key_id', 'password'])    
-
-    url = GH_API_URL %  "user/keys/%s" % self.key_id
-    headers = self.get_auth_header(self.user, self.password)
-    response, info = fetch_url(self.module, url, headers=headers)
-
-    return self.handle_response(response, info)
+      'present' : self.add_key,
+      'absent'  : self.remove_key
+    }[self.state]()
 
   def add_key(self):
-    self.validate_fields('add_key', ['title', 'key', 'password'])
-
     f = open(self.key, 'r')
     try:
       content = f.readline()
@@ -158,8 +133,6 @@ class GHKeys(object):
     return self.handle_response(response, info)
 
   def remove_key(self):
-    self.validate_fields('remove_key', ['key_id', 'password'])
-
     url = GH_API_URL %  "user/keys/%s" % self.key_id
     headers = self.get_auth_header(self.user, self.password)
     response, info = fetch_url(self.module, url, headers=headers, method='DELETE')
@@ -173,38 +146,43 @@ class GHKeys(object):
     }
     return headers
 
-  def validate_fields(self, action, fields):
-    for field in fields:
-      if getattr(self, field) is None:
-        raise ValueError(field + " cannot be null for action [" + action + "]")
-
   def handle_response(self, response, info):
     if info['status'] == 200:
       return response.read()
     else:
-      raise RuntimeError(info['msg']) 
+      raise RuntimeError('%s%s' % (info['msg'], \
+                                   " - key is already in use" if info['status'] == 422 else ""))
+
+def check_required_fields(module):
+  present_fields = ['title', 'key', 'password']
+  absent_fields = ['key_id', 'password']
+
+  fields = present_fields if module.params['state'] == 'present' else absent_fields
+  for field in fields:
+    if module.params[field] is None:
+      raise ValueError(field + " cannot be null for state [" + module.params['state'] + "]")
 
 def main():
   module = AnsibleModule(
     argument_spec = dict(
-      password = dict(),
+      password = dict(no_log=True),
       title    = dict(),
       key      = dict(),
       key_id   = dict(),
       user     = dict(required=True),
-      action   = dict(choices=['present', 'absent']),
-    )
+      state    = dict(choices=['present', 'absent'], default='present'),
+    ),
+    supports_check_mode=True
   )  
 
+  check_required_fields(module)
+  if module.check_mode:
+    module.exit_json(changed=True)
+
   gh_keys = GHKeys(module)
-  if gh_keys.action is None:
-    if gh_keys.key_id is not None:
-      gh_keys.action = "get_key"
-    else:
-      gh_keys.action = "list_keys"
 
   try:
-    response = gh_keys.perform_by_action()
+    response = gh_keys.handle_state()
     module.exit_json(changed=True, result=response)
   except (RuntimeError, ValueError), err:
     module.fail_json(msg=err.args)
